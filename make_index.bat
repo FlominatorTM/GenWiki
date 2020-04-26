7c35b152cff38fa5bd399a978e846b5efa9df093
Flominatortm <flominator@gmx.net>
2020-04-26 22:05:42 +0200

    double quotes for parameters

--- make_index.bat
+++ make_index.bat
@@ -1,7 +1,6 @@
 REM goto folder where this script lies
 pushd %~dp0
 REM attach _sorted.txt to the input file name
-python make_index.py "%1" > %~dpn1_sorted.txt
+python make_index.py "%1" > "%~dpn1_sorted.txt"
 REM run again in order to extract only the notes
-python make_index.py "%1" notes > %~dpn1_notes.txt
-pause
+python make_index.py "%1" notes > "%~dpn1_notes.txt"

75d31bfdd432a72799d0665676278adf5c083930
Flominatortm <flominator@gmx.net>
2020-04-30 17:44:06 +0200

    removing double quotes

--- make_index.bat
+++ make_index.bat
@@ -1,6 +1,10 @@
 REM goto folder where this script lies
 pushd %~dp0
+
+REM variable used to remove redundant double quotes
+set source=%1
+
 REM attach _sorted.txt to the input file name
-python make_index.py "%1" > "%~dpn1_sorted.txt"
+python make_index.py "%source:"=%" > "%~dpn1_sorted.txt"
 REM run again in order to extract only the notes
-python make_index.py "%1" notes > "%~dpn1_notes.txt"
+python make_index.py "%source:"=%" notes > "%~dpn1_notes.txt"
